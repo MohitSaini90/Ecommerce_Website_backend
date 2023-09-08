4b8736b1473f5a21c5efc84c5939ad326ac9d9c1
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-09-09 03:17:39 +0530

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -37,5 +37,6 @@ const productSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-
+// Create an index on the `createdAt` field(for loadmore functionality)
+productSchema.index({ createdAt: -1 });
 export default mongoose.model("products", productSchema);
